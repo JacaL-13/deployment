420d5d5912ea81d3e7e6cd6b96b25440d037e3f6
Jared Clyde <jarclyde@gmail.com>
2022-11-02 15:16:01 -0600

    trying to get css to work

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use('/css', express.static(path.join(__dirname, '../public/styles.css')))
 
 const {home} = require('./controllers/pageCtrl')
 
